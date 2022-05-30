51bde79f6a2f2c382dd1c2c620e28b5aee399cdf
Gri12388 <gri12388@yandex.ru>
2022-05-30 09:10:07 +0400

    feat: make layout of the authentication page

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Auth.scss';
+import mainLogo from '../../assets/images/logo.svg';
+import footerLogo from '../../assets/images/logo-invert.svg';
+import poster from '../../assets/images/poster.png';
 
 function Auth() {
   return (
-    <div>
-      <p className='txt'>Hello, world!</p>
+    <div className='Auth__container'>
+      <main className='Auth__main'>
+        <section className='Auth__img-section'>
+          <img src={poster} alt="poster" className='Auth__poster'/>
+        </section>
+        <section className='Auth__auth-section'>
+          <img src={mainLogo} alt="logotype" className='Auth__main-logo' />
+        </section>
+      </main>
+      <footer className='Auth__footer'>
+        <img src={footerLogo} alt="logotype" className='Auth__footer-logo' />
+      </footer>
     </div>
   );
 }
