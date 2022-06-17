63044aff6a8fe58beaff8a1ebd94cecbdcd0c31f
Gri12388 <gri12388@yandex.ru>
2022-06-17 15:13:57 +0400

    fix: refactor 'Sel' component

--- src/components/Sel/Sel.jsx
+++ src/components/Sel/Sel.jsx
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-
-import { selectTypes } from '../../store/slices/typesSlice.js';
 
 import '../../assets/styles/common.scss';
 import './Sel.scss';
 
-import arrowDown from '../../assets/images/arrow-down.svg';
+import sprite from '../../assets/images/sprite.svg';
 
 
-function Sel({id, label, value, groupId, state, callbacks, placeholder}) {
+
+function Sel({ id, label, value, groupId, state, callbacks, placeholder }) {
   
   let types = Object.values(JSON.parse(sessionStorage.getItem('types'))).filter(item => item.slug);
+  
   let [isVisible, setIsVisible] = useState(false);
   let [color, setColor] = useState(value ? types[value].color : 'transparent');
   let [content, setContent] = useState(value ? types[value].type : placeholder ?? `Select ${(label ?? 'item').toLowerCase()}`);
   let [display, setDisplay] = useState(value ? true : false);
   //debugger
-  const toggleVisibility = () => setIsVisible(!isVisible);
-  const onButtonClick = e => {
-    e.preventDefault();
-    toggleVisibility();
+  function toggleVisibility() {
+    setIsVisible(isVisible => !isVisible);
   }
-  const chooseItem = e => {
+  function onButtonClick(e) {
+    e.preventDefault();
+  }
+  function chooseItem(e) {
     let temp = +e.currentTarget.dataset.id;
     setColor(types[temp].color);
     setContent(types[temp].type);
@@ -30,16 +30,22 @@ function Sel({id, label, value, groupId, state, callbacks, placeholder}) {
     toggleVisibility();
     callbacks.onChange(types[temp].id.toString());
   }
-  const onOutClick = e => {
+  function onKeyDown(e) {
+    if (e.code === 'Enter' || e.key === 'Enter') {
+      if (content) {
+
+      }
+      else {
+        e.preventDefault();
+        toggleVisibility();
+      }
+      
+    }
+  } 
+  function onOutClick(e) {
     let temp = e.target.dataset.group;
     if (temp !== groupId) setIsVisible(false);
   }
-  const onKeyDown = e => {
-    if (e.code === 'Enter' || e.key === 'Enter') {
-      e.preventDefault();
-    }
-  }
-
   useEffect(() => {
     document.addEventListener('click', onOutClick);
     return () => document.removeEventListener('click', onOutClick);
@@ -47,12 +53,29 @@ function Sel({id, label, value, groupId, state, callbacks, placeholder}) {
 
   const options = types.map(item => {
     return (
-      <div key={item.id} className='input_wrapper Sel__item' onClick={chooseItem} data-id={item.id} data-group={groupId}>
-        <div className='Sel__select' data-group={groupId}>
-          <div className='Sel__mark-area' data-group={groupId}>
-            <div className='Sel__mark' style={{backgroundColor: item.color}} data-group={groupId}></div>
+      <div  key={ item.id } 
+            className='input_wrapper Sel__item' 
+            onClick={ chooseItem } 
+            data-id={ item.id } 
+            data-group={ groupId }
+      >
+        <div  className='Sel__select' 
+              data-group={ groupId }
+        >
+          <div  className='Sel__mark-area' 
+                data-group={ groupId }
+          >
+            <div  className='Sel__mark' 
+                  style={{ backgroundColor: item.color }} 
+                  data-group={ groupId }
+            >
+            </div>
           </div>
-          <p className='text3' data-group={groupId}>{item.type}</p>
+          <p  className='text3' 
+              data-group={ groupId }
+          >
+            { item.type }
+          </p>
         </div>
       </div>
     );
@@ -60,41 +83,53 @@ function Sel({id, label, value, groupId, state, callbacks, placeholder}) {
   //console.log (state)
   return(
     <>
-      {label && <label htmlFor={id} className='text1 InputText__label'>{label}</label>}
+      {label && <label htmlFor={ id } className='text1 InputText__label'>{ label }</label>}
       <div  className={ state && state.touched && !state.status ? 'input_wrapper Sel__base Sel__input_wrapper Sel__error' : 'input_wrapper Sel__base Sel__input_wrapper'} 
+            onClick={ toggleVisibility }
             data-group={groupId}
       >
-        <button className='Sel__select' 
-                onClick={onButtonClick} 
-                onBlur={callbacks.onBlur}
-                data-group={groupId}
+        <button className='Sel__select'
+                id={ id } 
+                onClick={ onButtonClick } 
+                onBlur={ callbacks.onBlur }
+                onKeyDown={ onKeyDown }
+                data-group={ groupId }
         >
           <div  className='Sel__mark-area' 
-                onClick={toggleVisibility} 
-                style={{display: value ? 'flex' : 'none'}} 
-                data-group={groupId}
+                style={{ display: value ? 'flex' : 'none' }} 
+                data-group={ groupId }
           >
             <div  className='Sel__mark' 
-                  style={{backgroundColor: value ? color : 'transparent'}} 
-                  data-group={groupId}
+                  style={{ backgroundColor: value ? color : 'transparent' }} 
+                  data-group={ groupId }
             ></div>
           </div>
-          <p className='text3' data-group={groupId}>{value ? content : placeholder ?? `Select ${(label ?? 'item').toLowerCase()}`}</p>
+          <p  className='text3' 
+              data-group={groupId}
+          >
+            {value ? content : placeholder ?? `Select ${(label ?? 'item').toLowerCase()}`}
+          </p>
         </button>
         <div  className='Sel__side-area' 
-              onClick={toggleVisibility} 
-              data-group={groupId}
+              data-group={ groupId }
         >
-          <img src={arrowDown} alt='arrow-down' data-group={groupId} />
+          <svg  className={ isVisible ? 'Sel__icon-rotated_svg' : 'Sel__icon_svg' } 
+                data-group={ groupId }
+          >
+            <use  href={ sprite + '#chevron' } 
+                  data-group={ groupId }
+            ></use>
+          </svg>
+          
         </div>
         <div  className='Sel__drop' 
-              style={{display: isVisible ? 'block' : 'none'}} 
-              data-group={groupId}
+              style={{ display: isVisible ? 'block' : 'none' }} 
+              data-group={ groupId }
         >
           {options}
         </div>
       </div>
-      <p className='text8'>{state && state.touched && !state.status && state.error}</p>
+      <p className='text8'>{ state && state.touched && !state.status && state.error }</p>
     </>
   );
 }
