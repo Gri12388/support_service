1d13db577a0520c1cf0222957cdbe1845f81c488
Gri12388 <gri12388@yandex.ru>
2022-06-24 04:01:31 +0400

    fix: refactor 'statusesSlice' and 'typesSlice' files

--- src/store/slices/statusesSlice.js
+++ src/store/slices/statusesSlice.js
@@ -13,13 +13,3 @@ const statusesSlice = createSlice({
 export const selectStatuses = state => Object.values(state.statuses);
 
 export default statusesSlice.reducer;
-
-//---------------------------
-
-// {
-//   0: {id: 0, status: 'NEW', color: '#6C5CE7'},
-//   1: {id: 1, status: 'DONE', color: '#00B894'},
-//   2: {id: 2, status: 'IN PROGRESS', color: '#FDCB6E'},
-//   3: {id: 3, status: 'DECLINED', color: '#E84393'},
-//   4: {id: 4, status: 'UNDEFINED', color: '#ADADAD'},
-// };
--- src/store/slices/typesSlice.js
+++ src/store/slices/typesSlice.js
@@ -13,13 +13,3 @@ const typesSlice = createSlice({
 export const selectTypes = state => Object.values(state.types);
 
 export default typesSlice.reducer;
-
-//--------------------------------------
-
-// {
-//   0: {id: 0, type: 'Hardware', color: '#7DB59A'},
-//   1: {id: 1, type: 'Software', color: '#FF7675'},
-//   2: {id: 2, type: 'Networking', color: '#FDCB6E'},
-//   3: {id: 3, type: 'Troubleshooting', color: '#6C5CE7'},
-//   4: {id: 4, type: 'Other', color: '#ADADAD'},
-// };
